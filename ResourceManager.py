a29252b6f1466da42f064539300c215c3df4eed2
Alexander-Klein-GVSU <kleialex@mail.gvsu.edu>
2021-11-16 21:15:50 -0500

    Restructured Code: DL Detection broken, need to create GUI

--- ResourceManager.py
+++ ResourceManager.py
@@ -1,112 +1,131 @@
-import Process
-import Resource
 import copy
 
+# Name: Alexander Klein
+# Date: 11/15/21
+# Assignment: Resource Manager
+
 #Function
-def checker(p, r, rl):
+def checker(has, want, available, total, finished, rl):
     #Initializes internal values.
-    prcCpy = copy.deepcopy(p)
-    resCpy = copy.deepcopy(r)
+    hasC = copy.deepcopy(has)
+    wantC = copy.deepcopy(want)
+    availC = copy.deepcopy(available)
+    totalC = copy.deepcopy(total)
+    finishC = copy.deepcopy(finished)
     finishedPrcs = 0
     stuckPrcs = 0
 
     #Checks if every process is finished
-    for prcs in prcCpy:
-        if (prcs.isFinished()):
+    for i in range(processes):
+        if (finishC[i]):
             finishedPrcs += 1
     if (finishedPrcs == processes):
         dl = False
     else:
-        iterations = 0
-        for prcs in prcCpy:
+        finishedPrcs = 0
+        stuckPrcs = 0
+        iterations = -1
+        for i in range(processes):
             iterations += 1
             #If the process is not finished.
-            if (not(prcs.isFinished())):
+            if (not(finishC[i])):
                 hasAll = 0
                 #Counts if the process has all of its resources, releasing them if it does.
-                for i in range(resources-1):
-                    if (prcs.getRequests(i) == 0):
+                for j in range(resources):
+                    if (wantC[i][j] == 0):
                         hasAll += 1
                 if (hasAll == resources):
-                    for i in range(resources-1):
-                        resCpy[i].returnRes(prcs.getHas(i))
-                        prcs.release(i, prcs.getHas(i))
+                    for j in range(resources):
+                        availC[j] += hasC[i][j]
+                        hasC[i][j] = 0
+                        finishC[i] = True
                     finishedPrcs += 1
                     runOption[rl] = iterations
                     rl += 1
-                    checker(prcCpy, resCpy, rl)
+                    checker(hasC, wantC, availC, totalC, finishC, rl)
                 #If the process doesnt have all of its processes.
                 else:
-                    for i in range(resources-1):
+                    for j in range(resources):
                         #Allocates the resources if they are available.
-                        if (resCpy[i].provideRes(prcs.getRequests(i)) == 0):
-                            prcs.take(i, prcs.getRequests(i))
-                        else:
-                            prcs.take(i, resource[i].getAvailable())
-                            resCpy[i].provideRes(resource[i].getAvailable())
+                        hasC[i][j] += min(wantC[i][j], availC[j])
+                        wantC[i][j] -= min(wantC[i][j], availC[j])
+                        availC[j] -= min(wantC[i][j], availC[j])
                 hasAll = 0
                 #Counts if the process has all of its resources, releasing them if it does.
-                for i in range(resources-1):
-                    if (prcs.getRequests(i) == 0):
+                for j in range(resources):
+                    if (wantC[i][j] == 0):
                         hasAll += 1
                 if (hasAll == resources):
-                    checker(prcCpy, resCpy, rl)
+                    checker(hasC, wantC, availC, totalC, finishC, rl)
                 else:
                     stuckPrcs += 1
+            else:
+                finishedPrcs += 1
     if (not(finishedPrcs == processes) and (finishedPrcs + stuckPrcs == processes)):
         dl = True
 
 #Opens the file.
 file = open("upload.txt", "r")
+input = file.read()
+input = input.split('\n')
+
+i = 0
+for l in input:
+    input[i] = l.split(' ')
+    i += 1
 
 #Gets the number of processes.
-input = file.readline(0)
-processes = int(input)
+processes = int(input[0][0])
 
 #Gets the number of Resources.
-input = file.readline(1)
-input = input.split(" ")
-resources = input[0]
+resources = int(input[1][0])
 
 #Declares the processes and resources.
-resource = []
-process = []
+availableR = [0 for i in range(resources)]
+totalR = [0 for i in range(resources)]
+wantP = [[0 for i in range(resources)] for j in range(processes)]
+hasP = [[0 for i in range(resources)] for j in range(processes)]
+pFinished = [False for i in range(processes)]
 
 #Generates the resources.
-for i in range(1, resources):
-    resource[i-1] = Resource(input[i])
+for i in range(1, resources+1):
+    availableR[i-1] = (int(input[1][i]))
+    totalR[i-1] = (int(input[1][i]))
 
-#Generates the processes.
-for i in range(processes-1):
-    process[i] = Process(resources)
+#converts numerical input to integers.
+for i in range(2, len(input)):
+    for j in range(1, 4):
+        input[i][j] = int(input[i][j])
 
 #Runs the main part of the program.
-lineCt = len(file.readlines)
-for i in range(2, lineCt-1):
-    input = file.readline(i)
-    input = input.split(" ")
-
+for i in range(2, len(input)):
     #If the current command is a request of resources.
-    if (input[0] == "request"):
+    if (input[i][0] == 'request'):
         #Checks if the amount of resources requested is possible to be allocated.
-        if (resource[input[2]].getTotal() < input[3]):
+        if (totalR[input[i][2]] < input[i][3]):
             print("This amount of this resource will never be available.\n")
         else:
             #Requests the number of resources by the process.
-            process[input[1]].request(input[2], input[3])
+            wantP[input[i][1]][input[i][2]] += input[i][3]
+            pFinished[input[i][1]] = False
+    
         #Allocates the resources if they are available.
-        if (resource[input[2]].provideRes(input[3]) == 0):
-            process[input[1]].take(input[2], input[3])
-        else:
-            process[input[1]].take(input[2], resource[input[2]].getAvailable())
-            resource[input[2]].provideRes(resource[input[2]].getAvailable())
+        hasP[input[i][1]][input[i][2]] += min(input[i][3], availableR[input[i][2]])
+        wantP[input[i][1]][input[i][2]] -= min(input[i][3], availableR[input[i][2]])
+        availableR[input[i][2]] -= min(input[i][3], availableR[input[i][2]])
     #If the current command is a release of resources.
-    elif (input[0] == "release"):
-        if (process[input[1]].getHas(input[2]) < input[3]):
+    elif (input[i][0] == 'release'):
+        if (hasP[input[i][1]][input[i][2]] < input[i][3]):
             print("You are attempting to release more resources than this process has.\n")
         else:
-            process[input[1]].release(input[3])
-            resource[input[2]].returnRes(input[3])
+            hasP[input[i][1]][input[i][2]] -= input[i][3]
+            availableR[input[i][2]] += input[i][3]
+        for k in range(processes):
+            for l in range(resources):
+                #Reallocates the resources if they are available.
+                        hasP[k][l] += min(wantP[k][l], availableR[l])
+                        wantP[k][l] -= min(wantP[k][l], availableR[l])
+                        availableR[l] -= min(wantP[k][l], availableR[l])
     #Invalid command.
     else:
         print("Error: Invalid Command\n")
@@ -114,9 +133,9 @@ for i in range(2, lineCt-1):
     #Checks if the processes can all be completed.
     
     dl = False
-    runOption = []
+    runOption = [0 for i in range(processes)]
     runLocation = 0
-    checker(process, resource, runLocation)
+    checker(hasP, wantP, availableR, totalR, pFinished, runLocation)
     if (dl):
         print("Processes are stuck in a deadlock.\n")
     else:
